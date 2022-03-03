d003a822a5320ac7a340e12c280e61c46477a35c
Marcos Gabriel <marcosdly@pm.me>
2022-03-03 15:35:22 -0300

    First custom config (not tested)

--- config.def.h
+++ config.def.h
@@ -1,17 +1,17 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "firacode:size=10", "fontawesome:size=10" };
+static const char dmenufont[]       = "firacode:size=10";
+static const char col_gray1[]       = "#080808"; // background
+static const char col_gray2[]       = "#444444"; // inactive window border
+static const char col_gray3[]       = "#b2b2b2"; // inactive foreground (font)
+static const char col_gray4[]       = "#eeeeee"; // active tag/window foreground (font)
+static const char col_cyan[]        = "#85dc85"; // active window border/secondary bar color
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -19,16 +19,18 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* Check https://dwm.suckless.org/customisation/tagmask/ to learn about tag masks */
+
+	/* class      		instance    title       tags mask     isfloating   monitor */
+	{ "galculator",     NULL,       NULL,       0,            1,           -1 },
+	{ "featherpad",  	NULL,       NULL,       0,		      1,           -1 },
 };
 
 /* layout(s) */
@@ -58,11 +60,14 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *rofimaincmd[] = { "exec", "$HOME/.rofi/apps", NULL };
+static const char *rofiruncmd[]  = { "exec", "$HOME/.rofi/run", NULL };
+static const char *termcmd[]     = { "kitty", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_d,      spawn,          {.v = rofimaincmd } },
+	{ MODKEY|ShiftMask,             XK_d,      spawn,          {.v = rofiruncmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
