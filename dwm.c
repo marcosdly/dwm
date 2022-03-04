a758e7f61667b2375cc46b7dde96ad5a8c98fdec
Marcos Gabriel <marcosdly@pm.me>
2022-03-04 16:58:29 -0300

    Revert "Patch: defaultlayoutpermonitor"
    This reverts commit 07ef2b8a6dfc4875876d4498dc473d20d4141dba.

--- dwm.c
+++ dwm.c
@@ -164,7 +164,6 @@ struct Systray {
 };
 
 /* function declarations */
-static void applydefaultlayouts();
 static void applyrules(Client *c);
 static int applysizehints(Client *c, int *x, int *y, int *w, int *h, int interact);
 static void arrange(Monitor *m);
@@ -349,21 +348,6 @@ autostart_exec() {
 }
 
 /* function implementations */
-void
-applydefaultlayouts()
-{
-    Monitor *m;
-    int i = 0;
-    for (m = mons; m; m = m->next) {
-        if (i < LENGTH(lpm)) {
-            m->lt[0] = &layouts[lpm[i]];
-            m->lt[1] = &layouts[(lpm[i] + 1)/ % LENGTH(layouts)];
-            strncpy(m->ltsymbol, layouts[i].symbol, sizeof m->ltsymbol);
-        }
-        i++;
-    }
-}
-
 void
 applyrules(Client *c)
 {
@@ -2333,7 +2317,6 @@ updategeom(void)
 				cleanupmon(m);
 			}
 		}
-        applydefaultlayouts();
 		free(unique);
 	} else
 #endif /* XINERAMA */
